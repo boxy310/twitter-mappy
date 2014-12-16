77222b2712eb342be8fa7e8993a39db01abfda4d
Andrew Troemner <ajtroemn@iupui.edu>
2014-12-15 22:49:09 -0500

    Create dbschema.py

---
+++ dbschema.py
@@ -0,0 +1,28 @@
+import sqlite3
+from twython import Twython
+
+#connect to DB
+conn = sqlite3.connect("twitter.db")
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE user (
+id INT,
+screen_name VARCHAR(15),
+name VARCHAR(20),
+description TEXT,
+location VARCHAR(50),
+url VARCHAR(50),
+friends_count INT,
+followers_count INT,
+statuses_count INT,
+lang VARCHAR(5),
+geo_enabled BOOL,
+twitter_date_created SHORTDATETIME,
+date_created SHORTDATETIME,
+date_last_modified SHORTDATETIME,
+id_str VARCHAR(15),
+date_sync SHORTDATETIME
+) """)
+
+cursor.execute("CREATE TABLE follower (user_id INT, follower_id INT)")
+conn.commit()
